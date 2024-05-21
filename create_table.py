8e6e519119dd54581b4c71f27412dd7ca74b41af
Nicolas Descarpentries <nicolas.descarp@outlook.fr>
2024-05-21 15:31:16 +0200

    Base de la DB

--- create_table.py
+++ create_table.py
@@ -12,4 +12,33 @@ cursor.execute("""
     );
 """)
 
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Parking (
+        Park_Id INTEGER PRIMARY KEY AUTOINCREMENT,
+        Nom_Parking VARCHAR UNIQUE,
+        Adresse_Parking VARCHAR,
+        Ville_Parking VARCHAR  
+    );
+""")
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Places (
+        Place_Id INTEGER PRIMARY KEY AUTOINCREMENT,
+        Nom_Place VARCHAR UNIQUE,
+        Park_id INTEGER UNIQUE,
+        FOREIGN KEY(Park_id) REFERENCES Parking(Park_Id)       
+    );
+""")
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS User_Parking (
+        User_Parking_id INTEGER PRIMARY KEY,
+        User_id INTEGER,
+        Park_id INTEGER,
+        FOREIGN KEY(Park_id) REFERENCES Parking(Park_Id),
+        FOREIGN KEY(User_id) REFERENCES User(User_id)       
+    );
+""")
+
+
 connection.commit()
